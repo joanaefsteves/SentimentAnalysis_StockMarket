f12eec5c3d01583c7636fb7cd8a1eec552ca35f0
Joana Esteves <joana.f.r.esteves@gmail.com>
2025-06-13 01:29:26 +0100

    preprocessing, finbert

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,6 +1,7 @@
 
 import re, string
 import emoji
+import contractions
 import torch
 import pandas as pd
 
@@ -10,11 +11,12 @@ from nltk.tokenize import word_tokenize # type: ignore
 from nltk.stem import WordNetLemmatizer, PorterStemmer # type: ignore
 
 from transformers import MarianMTModel, MarianTokenizer
-from transformers import BertTokenizer
 
 import langid
 
 from emoji import demojize # type: ignore
+
+from abc import ABC, abstractmethod
 
 
 """
@@ -22,9 +24,29 @@ from emoji import demojize # type: ignore
     ascii 97-122
 """
 
-class Preprocessing:
-    def __init__(self, lemmatize=True, stem=False, text_col='text', emoji_support_level=0, translate=False):
-        self.text_col = text_col
+class PreprocessingBase(ABC):
+    def __init__(self):
+        pass
+
+    def preprocess(self, texts):
+        return [self._clean(text) for text in texts]
+
+    @abstractmethod
+    def _clean(self, text):
+        pass
+
+    @abstractmethod
+    def _translate(self, text):
+        pass
+
+    @abstractmethod
+    def _demojize(self, text):
+        pass
+
+#--------- Main preprocessing -----------#
+
+class Preprocessing(PreprocessingBase):
+    def __init__(self, lemmatize=True, stem=False, emoji_support_level=0, translate=False):
         self.translate = translate
         
         self._download_nltk_data()
@@ -42,18 +64,20 @@ class Preprocessing:
         
         # Dictionary of regex patterns and their normalized replacements
         self.normalization_patterns = {
-            r'\n': ' ',                             # line breaks 
-            r'[$€£]\d+(?:[\.,]?\d+)?': 'COST',      # costs
-            r'http\S+|www\S+|https\S+': 'URL',      # urls
-            r'#\w+': 'HASHTAG',                      # hashtags
-            r'@\w+': 'USER',                         # mentions
-            r'\$[A-Z]{1,6}\b': 'TICKER',             # tickers
-            r':\w+:': 'EMOJI',                        # emojis in text
-            r"\'": '',                             # remove single quotes
-            
+            r'\n': ' ',                                 # line breaks 
+            r'[$€£]\d+(?:[\.,]?\d+)?': 'COST',          # costs
+            r'http\S+|www\S+|https\S+': 'URL',          # urls
+            r'#\w+': 'HASHTAG',                         # hashtags
+            r'@\w+': 'USER',                            # mentions
+            r'\$[A-Z]{1,6}\b': 'TICKER',                # tickers
+            r':\w+:': 'EMOJI',                          # emojis in text
+            r'\b([a-zA-Z]+)-([a-zA-Z]+)': r'\1\2',      # remove hyphen in hyphenated words
+            r'\b([A-Z])\.(?=[A-Z]\b)': r'\1',           # Remove dots from acronyms 
+            r"'s": '',                                  # remove remaining 's (possesion)
+            r"\'": '',                                  # remove single quotes
+            r'\b[a-zA-Z]\b': ''                         # one char
         }
         
-        # STILL NEED TO ADAPT TO CONSIDER MORE LANGUAGES
         self.stopwords = set(stopwords.words("english"))
         self.english_vocab = set(w.lower() for w in words.words())
         self.lemmatizer = WordNetLemmatizer()
@@ -64,10 +88,10 @@ class Preprocessing:
         resources = [
             ('corpora/stopwords', 'stopwords'),
             ('corpora/words', 'words'),
-            ('tokenizers/punkt', 'punkt'),  # Tokenizer for word tokenization
+            ('tokenizers/punkt', 'punkt'),          # Tokenizer for word tokenization
             ('tokenizers/punkt_tab', 'punkt_tab'),  # Updated tokenizer
             ('corpora/wordnet', 'wordnet'),
-            ('corpora/omw-1.4', 'omw-1.4')  # Additional wordnet data
+            ('corpora/omw-1.4', 'omw-1.4')          # Additional wordnet data
         ]
         
         for resource_path, resource_name in resources:
@@ -77,50 +101,16 @@ class Preprocessing:
                 print(f"Downloading {resource_name}...")
                 nltk.download(resource_name, quiet=True)
 
-    def preprocess(self, texts):
-        return [self._clean_text(text) for text in texts]
-    
-    def final_ascii_clean(self, texts):
-        """Final cleaning to ensure all characters are ASCII"""
-        cleaned_texts_list = []
-        print(f"Initial texts: {texts}")  # Debugging line to print initial texts
-        for text in texts:
-            print(f"Processing text: {text}")  # Debugging line to print each text being processed
-            print(f"ord values: {[ord(char) for char in text]}")  # Debugging line to print ord values of characte
-            cleaned_text = []
-            for char in text:
-                if 65 <= ord(char) <= 90 or 97 <= ord(char) <= 122:
-                    cleaned_text.append(char)
-            cleaned_texts_list.append(''.join(cleaned_text))
-        return cleaned_texts_list
-    
-    def _translate(self, text, threshold=0.9):
-  
-        if not self.translate:
-            return text
-        try:
-            text_str = str(text)
-            lang, prob = langid.classify(text_str)
-            tokens = text_str.lower().split()
-            has_stops = sum(word in self.stopwords for word in tokens) >= 2
-            has_vocab = sum(word in self.english_vocab for word in tokens) >= 1
-
-            # Força para inglês se baixa confiança mas parecer inglês
-            if prob < threshold and (has_stops or has_vocab):
-                lang = 'en'
-
-            if lang != 'en':
-                inputs = self.tokenizer([text_str], return_tensors="pt", padding=True, truncation=True)
-                with torch.no_grad():
-                    generated = self.model.generate(**inputs)
-                return self.tokenizer.decode(generated[0], skip_special_tokens=True)
-        except Exception as e:
-            print(f"Translation failed: {e}")
-        return text 
-
-    def _clean_text(self, text):
+    def _clean(self, text):
         
         text = text.lower()  # Convert to lowercase
+
+        text = demojize(text) # Add demojize pattern 
+
+        text = self._fix_elongations(text)  # Fix elongations
+
+        text = self._expand_contractions(text) # Expand contractions
+
         # Apply normalization patterns
         for pattern, replacement in self.normalization_patterns.items():
             if replacement == 'EMOJI' and self.emoji_support_level > 0:
@@ -130,16 +120,13 @@ class Preprocessing:
             else:
                 text = re.sub(pattern, replacement, text)
 
-
-        text = self._fix_elongations(text)  # Fix elongations
-
         if self.translate: 
             text = self._translate(text)  # Translate if needed
-        # Tokenize and lowercase
-        
-        ascii_text  = self.final_ascii_clean(text)
+
+        # Clean
+        ascii_text  = self._final_ascii_clean(text)
         print(f"ASCII cleaned text: {ascii_text}")  # Debugging line to print ASCII cleaned text
-        tokens = word_tokenize(ascii_text)
+        tokens = self._tokenize(ascii_text)
 
         # Remove punctuation and stopwords
         tokens = [t for t in tokens if (t not in self.punctuation and t not in self.stopwords)]
@@ -162,20 +149,60 @@ class Preprocessing:
 
         return processed_tokens
     
+    def _final_ascii_clean(self, text):
+        """Final cleaning to ensure all characters are ASCII"""
+        cleaned_text = []
+        for char in text:
+            if (65 <= ord(char) <= 90) or (97 <= ord(char) <= 122):
+                cleaned_text.append(char)
+            else:
+                cleaned_text.append(' ')
+        return ''.join(cleaned_text)
+    
+    def _tokenize(self, text):
+        tokens = word_tokenize(text)
+        return tokens
+    
+    def _translate(self, text, threshold=0.9):
+  
+        if not self.translate:
+            return text
+        try:
+            text_str = str(text)
+            lang, prob = langid.classify(text_str)
+            tokens = text_str.lower().split()
+            has_stops = sum(word in self.stopwords for word in tokens) >= 2
+            has_vocab = sum(word in self.english_vocab for word in tokens) >= 1
+
+            if prob < threshold and (has_stops or has_vocab):
+                lang = 'en'
+
+            if lang != 'en':
+                inputs = self.tokenizer([text_str], return_tensors="pt", padding=True, truncation=True)
+                with torch.no_grad():
+                    generated = self.model.generate(**inputs)
+                return self.tokenizer.decode(generated[0], skip_special_tokens=True)
+        except Exception as e:
+            print(f"Translation failed: {e}")
+        return text 
+    
     def _get_valid_elongations(self, text):
-        
         return [m.group(0) for m in self.elongation_pattern.finditer(str(text))]
 
     def _fix_elongations(self, text):
-        
         elong_words = self._get_valid_elongations(text)
         for word in elong_words:
             fixed = re.sub(r'(.)\1{2,}', r'\1\1', word)
             text = text.replace(word, fixed)
         return text
+    
+    def _expand_contractions(self, text):
+        # Applies contraction expansion 
+        text = contractions.fix(text)
+        return text
 
-    def demojize(self, df):
-        df[self.text_col] = df[self.text_col].apply(lambda x: demojize(str(x)))
+    def _demojize(self, text):
+        return demojize(text)
     
     def get_stopwords(self):
         """Get the set of stopwords used in preprocessing"""
@@ -195,3 +222,49 @@ class Preprocessing:
     def get_normalized_patterns(self):
         """Get the normalized patterns used in preprocessing"""
         return list(self.normalization_patterns.values())
+    
+#--------- Preprocessing for trained models - light -----------#
+
+class PreprocessingPretrained(PreprocessingBase):
+    def __init__(self, translate=True):
+        self.translate = translate 
+        self.translator_name = "Helsinki-NLP/opus-mt-mul-en"
+        self.stopwords = set(stopwords.words("english"))
+        self.english_vocab = set(w.lower() for w in words.words())
+        self.tokenizer = MarianTokenizer.from_pretrained(self.translator_name)
+        self.model = MarianMTModel.from_pretrained(self.translator_name)
+        
+    def _clean(self, text):
+
+        text = demojize(text) # Demojize
+
+        if self.translate: 
+            text = self._translate(text)  # Translate if needed
+
+        return text
+    
+    def _translate(self, text, threshold=0.9):
+  
+        if not self.translate:
+            return text
+        try:
+            text_str = str(text)
+            lang, prob = langid.classify(text_str)
+            tokens = text_str.lower().split()
+            has_stops = sum(word in self.stopwords for word in tokens) >= 2
+            has_vocab = sum(word in self.english_vocab for word in tokens) >= 1
+
+            if prob < threshold and (has_stops or has_vocab):
+                lang = 'en'
+
+            if lang != 'en':
+                inputs = self.tokenizer([text_str], return_tensors="pt", padding=True, truncation=True)
+                with torch.no_grad():
+                    generated = self.model.generate(**inputs)
+                return self.tokenizer.decode(generated[0], skip_special_tokens=True)
+        except Exception as e:
+            print(f"Translation failed: {e}")
+        return text 
+
+    def _demojize(self, text):
+        return demojize(text)
